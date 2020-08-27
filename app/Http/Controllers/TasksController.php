c58f0198918998acf6cc06e3c1c72b64819412a7
Ubuntu <ubuntu@ip-172-31-6-120.ap-northeast-1.compute.internal>
2020-08-27 08:51:59 +0000

    profile

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -103,6 +103,7 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        
         $request->validate([
            'status'=>'required|max:10',
            'content'=>'required|max:255',
